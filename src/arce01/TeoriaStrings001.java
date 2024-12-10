2fe5a89696aaf8f733fda4fc088a413fcee26ab0
Jojoseja <jfeyjoo@gmail.com>
2024-12-10 08:55:01 +0100

    Create TeoriaStrings001.java

---
+++ src/arce01/TeoriaStrings001.java
@@ -0,0 +1,9 @@
+package arce01;
+
+public class TeoriaStrings001 {
+	public static void main(String[] args) {
+		String cadena = "Buenos Dias";
+		char a = cadena.charAt(2);
+		System.out.println(a);
+	}
+}
