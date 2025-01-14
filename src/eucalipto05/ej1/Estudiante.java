6b87f2abf4ab99dd6920b2f4cb276e51eb8ec28d
Jojoseja <jfeyjoo@gmail.com>
2025-01-14 13:24:22 +0100

    Update

--- src/eucalipto05/ej1/Estudiante.java
+++ src/eucalipto05/ej1/Estudiante.java
@@ -1,16 +1,35 @@
 package eucalipto05.ej1;
 
 public class Estudiante {
+    private static int cont = 1;
+    private final int id;
     private String nombre;
     private double nota;
     private String dni;
 
     public Estudiante(String nombre, double nota, String dni) {
+        this.id = cont;
+        Estudiante.cont += 1;
         this.nombre = nombre;
         this.nota = nota;
         this.dni = dni;
     }
 
+    public static int getCont() {
+        return cont;
+    }
+
+    public static void setCont(int cont) {
+        Estudiante.cont = cont;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getID(){
+        return id;
+    }
     public String getDni() {
         return dni;
     }
@@ -35,5 +54,12 @@ public class Estudiante {
         this.nota = nota;
     }
 
-
+    public String toString() {
+        return "Estudiante{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", nota=" + nota +
+                ", dni='" + dni + '\'' +
+                '}';
+    }
 }
--- src/eucalipto05/ej1/Main.java
+++ src/eucalipto05/ej1/Main.java
@@ -7,7 +7,10 @@ public class Main {
         Estudiante es01 = new Estudiante("Carlos", 10,"15A");
         CuentaBancaria cuBa01 = new CuentaBancaria(20, es01);
         CuentaBancaria cuBa02 = new CuentaBancaria(10, 21, es01);
-        System.out.println(cuBa01);
-        System.out.println(cuBa02);
+        //System.out.println(cuBa01);
+        //System.out.println(cuBa02);
+        System.out.println(es01);
+        Estudiante es02 = new Estudiante("Paco", 15, "20B");
+        System.out.println(es02);
     }
 }
